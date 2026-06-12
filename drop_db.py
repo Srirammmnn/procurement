326d0e6f1148a6e8b443e3bacc87cd7ce3fedea7
Srirammmnn <sriramcr46@gmail.com>
2026-06-12 11:38:33 +0530

    i

--- drop_db.py
+++ drop_db.py
@@ -1,11 +1,9 @@
-from app.core.database import engine, Base
+from app.core.database import engine
 from sqlalchemy import text
 
 with engine.connect() as conn:
     conn.execute(text("DROP SCHEMA public CASCADE;"))
     conn.execute(text("CREATE SCHEMA public;"))
-    conn.execute(text("GRANT ALL ON SCHEMA public TO postgres;"))
-    conn.execute(text("GRANT ALL ON SCHEMA public TO public;"))
     conn.commit()
 
 print("Schema dropped and recreated.")
